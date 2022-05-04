08bcbe3968d5ee1f4fb3bc5b70f02ce866f56634
Veidz <joaovictorveidz@gmail.com>
2022-05-04 20:07:56 -0300

    general fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Core.Singleton;
 using DG.Tweening;
+using Core.Singleton;
 
 public class GameManager : Singleton<GameManager>
 {
