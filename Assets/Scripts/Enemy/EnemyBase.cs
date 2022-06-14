b429f638edde23d94c30cbfca7dd8ca298be3925
Veidz <joaovictorveidz@gmail.com>
2022-06-14 06:27:15 -0300

    create flash color script

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -6,6 +6,9 @@ public class EnemyBase : MonoBehaviour
 {
     public int damage = 10;
 
+    public Animator enemyAnimator;
+    public string triggerAttack = "Attack";
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         HealthBase health = collision.gameObject.GetComponent<HealthBase>();
@@ -13,6 +16,12 @@ public class EnemyBase : MonoBehaviour
         if (health)
         {
             health.Damage(damage);
+            PlayAttackAnimation();
         }
+    }
+
+    private void PlayAttackAnimation()
+    {
+        enemyAnimator.SetTrigger(triggerAttack);
     }
 }
