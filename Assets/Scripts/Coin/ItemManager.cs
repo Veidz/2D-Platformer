c9839fa359c15d86f73c30fa298f6b387f4ffadb
Veidz <joaovictorveidz@gmail.com>
2022-06-26 14:46:26 -0300

    add life config item manager

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -6,6 +6,7 @@ using Core.Singleton;
 public class ItemManager : Singleton<ItemManager>
 {
     public SOInt coins;
+    public SOInt lifes;
 
     private void Start()
     {
@@ -15,10 +16,16 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins.value = 0;
+        lifes.value = 3;
     }
 
     public void AddCoins(int amount = 1)
     {
         coins.value += amount;
+    }
+
+    public void AddLifes(int amount = 1)
+    {
+        lifes.value += amount;
     }
 }
